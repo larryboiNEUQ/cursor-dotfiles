002e161b63339936a3184e4d4e07a68d1bb4872a
larryboiNEUQ <836853129@qq.com>
2026-09-05 23:39:38 +0800

    Support Windows PowerShell file comparison

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -5,6 +5,26 @@ $RepoRoot = Split-Path -Parent $ScriptDir
 $DestinationRoot = if ($env:CURSOR_CONFIG_HOME) { $env:CURSOR_CONFIG_HOME } else { Join-Path $HOME '.cursor' }
 $Timestamp = Get-Date -Format 'yyyyMMddHHmmss'
 
+function Test-FileContentEqual {
+    param(
+        [Parameter(Mandatory = $true)][string]$Left,
+        [Parameter(Mandatory = $true)][string]$Right
+    )
+
+    $LeftBytes = [System.IO.File]::ReadAllBytes($Left)
+    $RightBytes = [System.IO.File]::ReadAllBytes($Right)
+    if ($LeftBytes.Length -ne $RightBytes.Length) {
+        return $false
+    }
+
+    for ($Index = 0; $Index -lt $LeftBytes.Length; $Index++) {
+        if ($LeftBytes[$Index] -ne $RightBytes[$Index]) {
+            return $false
+        }
+    }
+    return $true
+}
+
 function Install-ManagedFile {
     param(
         [Parameter(Mandatory = $true)][string]$Source,
@@ -14,13 +34,10 @@ function Install-ManagedFile {
     $DestinationDirectory = Split-Path -Parent $Destination
     New-Item -ItemType Directory -Path $DestinationDirectory -Force | Out-Null
 
-    if (Test-Path -LiteralPath $Destination -PathType Leaf) {
-        $SourceHash = (Get-FileHash -Algorithm SHA256 -LiteralPath $Source).Hash
-        $DestinationHash = (Get-FileHash -Algorithm SHA256 -LiteralPath $Destination).Hash
-        if ($SourceHash -eq $DestinationHash) {
-            Write-Host "unchanged  $Destination"
-            return
-        }
+    if ((Test-Path -LiteralPath $Destination -PathType Leaf) -and
+        (Test-FileContentEqual -Left $Source -Right $Destination)) {
+        Write-Host "unchanged  $Destination"
+        return
     }
 
     if (Test-Path -LiteralPath $Destination) {
